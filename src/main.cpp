20d51fb631b510c6c25dd9b794b0ecaae6f8044f
unknown <maximilian.gerhardt@rub.de>
2021-07-14 23:30:17 +0200

    Update example for recent code changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#if 0
 int led = PIN_LED;
 
 void setup() {
@@ -13,4 +14,47 @@ void loop() {
   delay(200);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(200);               // wait for a second
+}
+#endif 
+
+#include <LittleFS.h>
+#include <strings.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(5000);
+  LittleFS.begin();
+  char buff[32];
+  int cnt = 1;
+  File f = LittleFS.open("counts.txt", "r");
+  if (f) {
+    bzero(buff, 32);
+    if (f.read((uint8_t *)buff, 31)) {
+      sscanf(buff, "%d", &cnt);
+      Serial.printf("I have been run %d times\n", cnt);
+    }
+    f.close();
+  }
+
+  cnt++;
+  sprintf(buff, "%d\n", cnt);
+  f = LittleFS.open("counts.txt", "w");
+  if (f) {
+    f.write(buff, strlen(buff));
+    f.close();
+  }
+
+  Serial.println("---------------");
+  File i = LittleFS.open("file1.txt", "r");
+  if (i) {
+    while (i.available()) {
+      Serial.write(i.read());
+    }
+    Serial.println("---------------");
+    i.close();
+  }
+}
+
+
+void loop() {
 }
