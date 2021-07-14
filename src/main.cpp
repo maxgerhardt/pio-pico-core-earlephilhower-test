83ecb00f9ea82baabf36bc2446ee0385787e6585
unknown <maximilian.gerhardt@rub.de>
2021-07-14 05:08:33 +0200

    updae files

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@ int led = PIN_LED;
 
 void setup() {
   pinMode(led, OUTPUT);
- // Serial.begin(115200);
+  Serial.begin(115200);
 }
 
 void loop() {
-//  Serial.println("Test");
+  Serial.println("Test");
 //  delay(1000);
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
+  delay(200);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  delay(200);               // wait for a second
 }
